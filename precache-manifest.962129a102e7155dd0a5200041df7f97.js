cc4a4e8643fc61653176d931ed6c8fb859c12c27
Italo <cespinoza@disnovo.com>
2020-08-05 16:40:51 -0500

    Updates

---
+++ precache-manifest.962129a102e7155dd0a5200041df7f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c7866941226782d1647118509dcd7c",
+    "url": "/goodmorningdev/index.html"
+  },
+  {
+    "revision": "c65267c978d8efdeeece",
+    "url": "/goodmorningdev/static/css/main.9386bf47.chunk.css"
+  },
+  {
+    "revision": "aeb03fb996b59635f9d4",
+    "url": "/goodmorningdev/static/js/2.3a5faa7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodmorningdev/static/js/2.3a5faa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65267c978d8efdeeece",
+    "url": "/goodmorningdev/static/js/main.640fadb3.chunk.js"
+  },
+  {
+    "revision": "64493fb23898824b815a",
+    "url": "/goodmorningdev/static/js/runtime-main.19df0267.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goodmorningdev/static/media/logo.5d5d9eef.svg"
+  }
+]);
